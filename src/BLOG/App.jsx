b3bb165d56f396b00ad34c4ef0264b23fa71ea42
Abhinav <abhinav0606june@gmail.com>
2022-07-02 20:02:18 +0530

    final

--- src/BLOG/App.jsx
+++ src/BLOG/App.jsx
@@ -6,20 +6,34 @@ import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useContext } from "react";
+import { useContext,useEffect } from "react";
 import { Context } from "./context/Context";
 import Contact from "./pages/contact/Contact";
 import About from "./pages/About/About";
+import axios from "axios";
+
+// const URL_BACKEND = "https://blog-site-restapi.herokuapp.com/api/"
 
 function App() {
   const {user} = useContext(Context)
+
+  
+  // useEffect(() => {
+  //   const fetchPosts = async () => {
+  //     const res = await axios.get(URL_BACKEND);
+  //     console.log(res)
+  //   };
+  //   fetchPosts();
+  //   console.log("yo")
+  // }, []);
+
   return (
     <Router>
       {user?<Topbar />:<></>}
       {/* <Topbar /> */}
       <Routes>
-        {/* <Route exact path="/" element={<Homepage/>} /> */}
-        <Route exact path="/" element={user ? <Homepage /> : <Login />}/>
+        <Route exact path="/" element={<Homepage/>} />
+        {/* <Route exact path="/" element={user ? <Homepage /> : <Login />}/> */}
         <Route path="/posts" element={user ? <Homepage /> : <Login />}/>
         <Route path="/register" element={user ? <Homepage />:<Register />}/>
         <Route path="/login" element={user ? <Homepage /> :<Login />}/>
